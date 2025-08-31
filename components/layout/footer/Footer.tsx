78dc22af7110e0eb628bc091c3876ceee094a029
UmitAkbas <umit.akbas@hotmail.com.tr>
2025-08-31 17:53:18 +0300

    header and footer designed

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,9 +1,28 @@
+
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <div className='bg-primary text-white py-6'>
+            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+                <div className='text-center md:text-left space-y-4'>
+                    <Link href="/" className='text-2xl text-white font-bold'>
+                        Logo
+                    </Link>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, cupiditate.</p>
+                </div>
+<nav className='md:mt-o mt-16 space-x-4'>
+     <Link href="/home" className='text-white font-bold hover:underline'> Home</Link>
+                    <Link href="/about" className='text-white font-bold hover:underline'> About</Link>
+                    <Link href="/blog" className='text-white font-bold hover:underline'> Blog</Link>
+                    <Link href="/contact" className='text-white font-bold hover:underline'> Contact</Link>
+</nav>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Footer
--- components/layout/header/header.tsx
+++ components/layout/header/header.tsx
@@ -1,15 +1,40 @@
+import { Button } from '@/components/ui/button'
+import { Menu, Search, User } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
-  return (
-    <div className='bg-secondary shadow-sm top-0'>
-        <div className="container mx-auto flex items-center justify-center px-4 py-3">
+    return (
+        <div className='bg-stone-600 shadow-sm top-0'>
+            <div className="container mx-auto flex items-center justify-between px-4 py-3">
+                <Link href="/" className='text-2xl text-white font-bold'>
+                    Logo
+                </Link>
 
-Header
+                <nav className='hidden md:flex space-x-6'>
+                    <Link href="/home" className='text-white font-bold hover:underline'> Home</Link>
+                    <Link href="/about" className='text-white font-bold hover:underline'> About</Link>
+                    <Link href="/blog" className='text-white font-bold hover:underline'> Blog</Link>
+                    <Link href="/contact" className='text-white font-bold hover:underline'> Contact</Link>
+                </nav>
+                <div>
+                    <div className="flex items-center space-x-2">
+                        <Button variant="ghost">
+                            <Search />
+                        </Button>
+                        <Button variant="ghost">
+                            <User />
+                        </Button>
+                        <Button variant="ghost" className='md:hidden'>
+                            <Menu />
+                        </Button>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
-        
-         </div>
-  )
+    )
 }
 
 export default Header
